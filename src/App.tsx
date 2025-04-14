56db61d4e21889cd6df5bad3b31f654c9ef6a6e3
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2025-04-14 18:52:11 +0300

    sprint 03 week 01: lesson 1 add, BrowserRouter, Routes, Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,29 @@
-import React, {useState} from 'react';
-import './App.css';
-import {Todolist} from './Todolist';
-import {v1} from 'uuid';
-
-export type TaskType = {
-  id: string
-  title: string
-  isDone: boolean
-}
-
-export type TodolistsType = {
-  id: string
-  title: string
-  filter: FilterValuesType
-}
-
-export type FilterValuesType = "all" | "active" | "completed";
+import React from "react";
+import styles from "./components/Site.module.css";
+import { PageOne } from "./components/pages/PageOne";
+import { PageTwo } from "./components/pages/PageTwo";
+import { PageThree } from "./components/pages/PageThree";
+import { Route, Routes } from "react-router";
+import { Error404 } from "./components/pages/Error404";
 
 function App() {
-
-  let [filter, setFilter] = useState<FilterValuesType>("all");
-
-  // let [tasks, setTasks] = useState([
-  //     {id: v1(), title: "HTML&CSS", isDone: true},
-  //     {id: v1(), title: "JS", isDone: true},
-  //     {id: v1(), title: "ReactJS", isDone: false},
-  //     {id: v1(), title: "Rest API", isDone: false},
-  //     {id: v1(), title: "GraphQL", isDone: false},
-  // ]);
-  // let [filter, setFilter] = useState<FilterValuesType>("all");
-
-  let todolistID1 = v1();
-  let todolistID2 = v1();
-
-  let [todolists, setTodolists] = useState<Array<TodolistsType>>([
-    {id: todolistID1, title: 'What to learn', filter: 'all'},
-    {id: todolistID2, title: 'What to buy', filter: 'all'},
-  ])
-
-  let [tasks, setTasks] = useState({
-    [todolistID1]: [
-      {id: v1(), title: "HTML&CSS", isDone: true},
-      {id: v1(), title: "JS", isDone: true},
-      {id: v1(), title: "ReactJS", isDone: false},
-      {id: v1(), title: "Rest API", isDone: false},
-      {id: v1(), title: "GraphQL", isDone: false},
-    ],
-    [todolistID2]: [
-      {id: v1(), title: "HTML&CSS2", isDone: true},
-      {id: v1(), title: "JS2", isDone: true},
-      {id: v1(), title: "ReactJS2", isDone: false},
-      {id: v1(), title: "Rest API2", isDone: false},
-      {id: v1(), title: "GraphQL2", isDone: false},
-    ]
-  });
-
-
-  function removeTask(todolistId: string, id: string) {
-    let filteredTasks = tasks[todolistId].filter(t => t.id != id);
-    setTasks({
-      ...tasks,
-      [todolistId]: filteredTasks
-    });
-  }
-
-  function addTask(todolistId: string, title: string) {
-    let newTask = {id: v1(), title: title, isDone: false};
-    let newTasks = [newTask, ...tasks[todolistId]];
-    setTasks({
-      ...tasks,
-      [todolistId]: newTasks
-    });
-  }
-
-  function changeStatus(todolistID: string, taskId: string, isDone: boolean) {
-    let task = tasks[todolistID].find(t => t.id === taskId);
-    if (task) {
-      task.isDone = isDone;
-    }
-
-    setTasks({
-      ...tasks,
-      [todolistID]: tasks[todolistID]
-    });
-  }
-
-  function changeFilter(todolistId: string, value: FilterValuesType) {
-    setTodolists(todolists.map((todolist: TodolistsType) => todolist.id === todolistId
-      ? {...todolist,filter: value}
-      : todolist
-    ))
-  }
-
   return (
-    <div className="App">
-
-      {todolists.map((todolist: TodolistsType) => {
-        let tasksForTodolist = tasks[todolist.id];
-        if (filter === "active") {
-          tasksForTodolist = tasks[todolist.id].filter((t: TaskType) => t.isDone === false)
-        }
-        if (filter === "completed") {
-          tasksForTodolist = tasks[todolist.id].filter(t => t.isDone === true);
-        }
-
-        return (
-          <Todolist title={todolist.title}
-                    todolistId={todolist.id}
-                    tasks={tasksForTodolist}
-                    removeTask={removeTask}
-                    changeFilter={changeFilter}
-                    addTask={addTask}
-                    changeTaskStatus={changeStatus}
-                    filter={todolist.filter}
-                    key={todolist.id}
-          />
-        )
-      })}
+    <div>
+      <div className={styles.header}>
+        <h1>HEADER</h1>
+      </div>
+      <div className={styles.body}>
+        <div className={styles.nav}>Здесь будет навигация</div>
+        <div className={styles.content}>
+          <Routes>
+            <Route path="/page1" element={<PageOne />} />
+            <Route path="/page2" element={<PageTwo />} />
+            <Route path="/page3" element={<PageThree />} />
+            <Route path="/*" element={<Error404 />} />
+          </Routes>
+        </div>
+      </div>
+      <div className={styles.footer}>abibas 2023</div>
     </div>
   );
 }
---
+++ src/components/pages/Error404.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Error404 = () => {
+  return <div>My Error404</div>;
+};
